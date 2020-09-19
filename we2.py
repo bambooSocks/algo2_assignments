6b97d364508c48265a29fa6ae30abef888bcec7f
Matej <majtan.matej@gmail.com>
2020-09-19 18:42:01 +0200

    written 2

---
+++ we2.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+X = [1, 4, 5, 7, 9]
+n = len(X)
+D = np.zeros((n, n))
+
+
+def dist(x, i, j):
+    if i == j:
+        return 1
+    else:
+        output = [0]
+        for l in reversed(range(i)):
+            if x[i] - x[l] < x[j] - x[i]:
+                if D[l, i] == 0:
+                    D[l, i] = dist(x, l, i)
+                output.append(D[l, i])
+        return max(output) + 1
+
+
+def findMaxScore(x):
+    output = [0]
+    i = len(x) - 1
+    for l in reversed(range(i)):
+        if D[l, i] == 0:
+            D[l, i] = dist(x, l, i)
+        output.append(D[l, i])
+    return max(output)
+
+
+print(findMaxScore(X))
+print(D)

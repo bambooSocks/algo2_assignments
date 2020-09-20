84f08db6f77f2051db51a36472c341b24462b1d2
cj154 <cj154014737@hotmail.com>
2020-09-20 19:43:03 +0200

    Code optimization

--- we2.py
+++ we2.py
@@ -1,32 +1,32 @@
 import numpy as np
 
-X = [1, 4, 5, 7, 9]
-n = len(X)
+x = [10, 11, 13, 16, 17, 18]
+n = len(x)
 D = np.zeros((n, n))
 
 
-def dist(x, i, j):
+def findMaxHelper(i, j):
     if i == j:
         return 1
     else:
-        output = [0]
+        output = []
         for l in reversed(range(i+1)):
             if D[l, i] == 0:
-                D[l, i] = dist(x, l, i)
+                D[l, i] = findMaxHelper(l, i)
             if x[i] - x[l] < x[j] - x[i]:
                 output.append(D[l, i])
         return max(output) + 1
 
 
-def findMaxScore(x):
-    output = [0]
+def findMaxScore():
+    output = []
     i = len(x) - 1
     for l in reversed(range(i+1)):
         if D[l, i] == 0:
-            D[l, i] = dist(x, l, i)
+            D[l, i] = findMaxHelper(l, i)
         output.append(D[l, i])
     return max(output)
 
 
-print(findMaxScore(X))
+print(findMaxScore())
 print(D)
